12bfd599ab0657e2d3cde85618b0b22ad00a6bb4
lanou3g <qiansexiangbei2015@163.com>
2015-04-16 11:34:54 +0800

    第一次提交

---
+++ CellFactory/CellFactory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CellFactory
+//
+//  Created by lanou3g on 15-4-16.
+//  Copyright (c) 2015年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
